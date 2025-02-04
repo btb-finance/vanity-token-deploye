181c8bc18bfdca884dd325bfa74fd3c433cfaf4a
Muhammed Shahbaz <111690356+ShahbazBaloch515@users.noreply.github.com>
2025-02-04 07:19:57 +0100

    ....

--- contracts/mocks/MyOFTMock.sol
+++ contracts/mocks/MyOFTMock.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
-import { MyOFT } from "../MyOFT.sol";
+import { BTBFinance } from "../BTBFinance.sol";
 
 // @dev WARNING: This is for testing purposes only
-contract MyOFTMock is MyOFT {
+contract MyOFTMock is BTBFinance {
     constructor(
         string memory _name,
         string memory _symbol,
         address _lzEndpoint,
         address _delegate
-    ) MyOFT(_name, _symbol, _lzEndpoint, _delegate) {}
+    ) BTBFinance(_name, _symbol, _lzEndpoint, _delegate, 1000000e18, block.chainid) {}
 
    
 }
